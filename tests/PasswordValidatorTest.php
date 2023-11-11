8614f560356093c463beb69f981c1e40e643c4a3
katarn <katarn@katarn.es>
2023-11-11 17:15:21 +0100

    refactor: test set up

--- tests/PasswordValidatorTest.php
+++ tests/PasswordValidatorTest.php
@@ -8,21 +8,25 @@ use PHPUnit\Framework\TestCase;
 
 class PasswordValidatorTest extends TestCase
 {
-    function test_returns_true_when_valid_password_given(): void {
-        $validator = new PasswordValidator();
+    private PasswordValidator $validator;
 
-        self::assertTrue($validator->validate('Contras_1'));
+    protected function setUp(): void
+    {
+        $this->validator = new PasswordValidator();
     }
 
-    function test_returns_false_when_given_password_has_less_than_9_characters(): void {
-        $validator = new PasswordValidator();
-
-        self::assertFalse($validator->validate('Contra_1'));
+    function test_returns_true_when_valid_password_given(): void
+    {
+        self::assertTrue($this->validator->validate('Contras_1'));
     }
 
-    function test_returns_false_when_given_password_without_upper_character(): void {
-        $validator = new PasswordValidator();
+    function test_returns_false_when_given_password_has_less_than_9_characters(): void
+    {
+        self::assertFalse($this->validator->validate('Contra_1'));
+    }
 
-        self::assertFalse($validator->validate('contras_1'));
+    function test_returns_false_when_given_password_without_upper_character(): void
+    {
+        self::assertFalse($this->validator->validate('contras_1'));
     }
 }
